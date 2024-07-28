b3da8f2fc699901aa63fa96ebb99ec3b0cec294f
dhananjeyan23 <jeyan.mech7@gmail.com>
2024-07-28 15:23:10 +0530

    ppp

---
+++ src/org/demo/Workers.java
@@ -0,0 +1,16 @@
+package org.demo;
+
+public class Workers {
+	String ename1="padma";
+	//create the 1st method empName
+		private void empName1() {
+		System.out.println("employeename : "+ename1);
+		}
+		//create the main method 
+		public static void main(String[] args) {
+		//classname objrefname = new classname()
+			Workers ei1 = new Workers();
+			ei1.empName1();
+
+}
+}
